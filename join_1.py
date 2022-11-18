61e45b1d32b739817b9524dcb97ec5d934c9ca9c
PrakashAnpat <prakashp17696@gmail.com>
2022-11-18 18:40:48 +0530

    join program

---
+++ join_1.py
@@ -0,0 +1,18 @@
+string_1 = "I like this program."
+print(string_1)
+b = string_1.split(" ")
+print(b)
+print(type(b))
+c = " ".join(b)
+print(c)
+print(type(c))
+string_2 = "I like this operation."
+print(string_2)
+d = (1,2,3,4)
+print(d)
+e = "@".join([string_1,string_2])
+print(e)
+x = [1,2,3,4,5]
+
+z = " ".join(x)
+print(z)
